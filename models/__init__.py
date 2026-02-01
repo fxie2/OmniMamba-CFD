cb2136c8b4108a1ead26de801a0e09cf98fe2ed4
Feng XIE <aresxf@163.com>
2026-02-01 13:29:08 +0800

    add two files named __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .omni_mamba_block import OmniMambaBlock
+from .network import OmniMambaCFDSolver
+
+__all__ = [
+    'OmniMambaBlock',
+    'OmniMambaCFDSolver'
+]
---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+from .physics_loss import PhysicsLoss
+from .boundary import apply_hard_bc
+
+__all__ = [
+    'PhysicsLoss',
+    'apply_hard_bc'
+]
